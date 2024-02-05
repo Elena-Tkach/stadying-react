7f451ee3880f739b8208db2fd795227c612c077a
‘Elena <tcachelena@gmail.com>
2024-02-05 11:09:14 +0600

    feat: Add useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [likes, setLikes] = useState(0);
+  const [value, setValue] = useState('');
+
+  const increment = () => {
+    setLikes(likes + 1)
+  };
+  const decrement = () => {
+    setLikes(likes - 1)
+  };
+
   return (
     <div className="App">
-      
+      <h1>{likes}</h1>
+      <h2>{value}</h2>
+      <input type="text" value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
