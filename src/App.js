f6619e6e59a57deee7614453b07718a4fa8ca680
‘Elena <tcachelena@gmail.com>
2024-02-08 00:45:53 +0600

    feat: Add sort post

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import PostList from "./components/PostList";
 import { postss } from './consts';
 import cl from './styles/App.module.css'
 import PostForm from "./components/PostForm";
+import MySelect from "./UI/select/MySelect";
 
 
 function App() {
   const [posts, setPosts] = useState(postss);
-  
+  const [selectedSort, setSelectedSort] = useState('');
+
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
   }
@@ -15,11 +17,25 @@ function App() {
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
   }
+
+  const sortPosts = (sort) => {
+    setSelectedSort(sort)
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+  }
  
   return (
     <div className={cl.App}>
       
       <PostForm create={createPost} formTitle={'Добавить пост'} />
+      <MySelect
+        value={selectedSort}
+        onChange={sortPosts}
+        defaultValue='Сортировка'
+        options={[
+          { value: 'title', name: 'По названию' },
+          { value: 'subtitle', name: 'По описанию'},
+        ]}
+      />
       {posts.length
         ?  <PostList remove={removePost} posts={ posts} title={'Список постов'} />
         : <h1 style={{color: 'blue'}}>Постов больше нет</h1>
