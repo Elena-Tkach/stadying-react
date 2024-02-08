55254adf101e30401029b84db08cfdf085b070f2
‘Elena <tcachelena@gmail.com>
2024-02-08 08:33:25 +0600

    feat: Add search form list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import PostList from "./components/PostList";
 import { postss } from './consts';
 import cl from './styles/App.module.css'
 import PostForm from "./components/PostForm";
 import MySelect from "./UI/select/MySelect";
+import MyInput from "./UI/input/MyInput";
 
 
 function App() {
   const [posts, setPosts] = useState(postss);
   const [selectedSort, setSelectedSort] = useState('');
+  const [searchQuery, setSearchquery] = useState('');
 
+ 
+  const sortedPosts = useMemo(() => {
+    console.log('dfd')
+    if (selectedSort) {
+          return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]));
+      }
+    return posts;
+  }, [selectedSort, posts]);
+
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery))
+  }, [searchQuery, sortedPosts])
+  
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
   }
@@ -20,13 +35,18 @@ function App() {
 
   const sortPosts = (sort) => {
     setSelectedSort(sort)
-    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+    // setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
   }
  
   return (
     <div className={cl.App}>
       
       <PostForm create={createPost} formTitle={'Добавить пост'} />
+      <MyInput
+        value={searchQuery}
+        onChange= {e => setSearchquery(e.target.value)}
+        placeholder="Поиск..."
+      />
       <MySelect
         value={selectedSort}
         onChange={sortPosts}
@@ -36,8 +56,8 @@ function App() {
           { value: 'subtitle', name: 'По описанию'},
         ]}
       />
-      {posts.length
-        ?  <PostList remove={removePost} posts={ posts} title={'Список постов'} />
+      {sortedAndSearchedPosts.length
+        ?  <PostList remove={removePost} posts={ sortedAndSearchedPosts} title={'Список постов'} />
         : <h1 style={{color: 'blue'}}>Постов больше нет</h1>
       }
      
