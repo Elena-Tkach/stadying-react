647a48acce9f2d0dca5a6be6ff7d2f6697d1c1c5
‘Elena <tcachelena@gmail.com>
2024-02-09 09:39:06 +0600

    fix: Add hooks

--- src/App.js
+++ src/App.js
@@ -6,24 +6,15 @@ import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./UI/modal/MyModal";
 import MyButton from "./UI/button/MyButton";
+import { usePosts } from "./hooks/usePosts";
 
 
 function App() {
   const [posts, setPosts] = useState(postss);
   const [filter, setFilter] = useState({ sort: '', query: '' });
   const [modal, setModal] = useState(false);
- 
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-          return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
-      }
-    return posts;
-  }, [filter.sort, posts]);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
-  }, [filter.query, sortedPosts])
-  
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false);
